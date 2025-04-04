9d730254511131564a2daaa1f18aa579a78305be
aessaputra <aessaputra@yahoo.com>
2025-04-04 19:18:32 +0000

    feat: transactions item resource

--- app/Filament/Resources/TransactionItemsResource.php
+++ app/Filament/Resources/TransactionItemsResource.php
@@ -10,92 +10,111 @@ use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Illuminate\Contracts\Support\Htmlable;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class TransactionItemsResource extends Resource
 {
-    protected static ?string $model = TransactionItems::class;
+  protected static ?string $model = TransactionItems::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+  protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-    public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-                Forms\Components\TextInput::make('transactions_id')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\TextInput::make('foods_id')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\TextInput::make('quantity')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\TextInput::make('price')
-                    ->required()
-                    ->numeric()
-                    ->prefix('$'),
-                Forms\Components\TextInput::make('subtotal')
-                    ->required()
-                    ->numeric(),
-            ]);
-    }
+  public static function ShouldRegisterNavigation(): bool
+  {
+    return false;
+  }
 
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->columns([
-                Tables\Columns\TextColumn::make('transactions_id')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('foods_id')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('quantity')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('price')
-                    ->money()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('subtotal')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-            ])
-            ->filters([
-                //
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
-    }
+  public static string $parentResource = TransactionResource::class;
 
-    public static function getRelations(): array
-    {
-        return [
-            //
-        ];
-    }
+  public static function getRecordTitle(?Model $record): string|Htmlable|null
+  {
+    return $record->title;
+  }
 
-    public static function getPages(): array
-    {
-        return [
-            'index' => Pages\ListTransactionItems::route('/'),
-            'create' => Pages\CreateTransactionItems::route('/create'),
-            'edit' => Pages\EditTransactionItems::route('/{record}/edit'),
-        ];
-    }
+  public static function canEdit(Model $record): bool
+  {
+    return false;
+  }
+
+  public static function canCreate(): bool
+  {
+    return false;
+  }
+
+  public static function form(Form $form): Form
+  {
+    return $form
+      ->schema([
+        Forms\Components\TextInput::make('transactions_id')
+          ->required()
+          ->numeric(),
+        Forms\Components\TextInput::make('foods_id')
+          ->required()
+          ->numeric(),
+        Forms\Components\TextInput::make('quantity')
+          ->required()
+          ->numeric(),
+        Forms\Components\TextInput::make('price')
+          ->required()
+          ->numeric()
+          ->prefix('$'),
+        Forms\Components\TextInput::make('subtotal')
+          ->required()
+          ->numeric(),
+      ]);
+  }
+
+  public static function table(Table $table): Table
+  {
+    return $table
+      ->columns([
+        Tables\Columns\TextColumn::make('created_at')
+          ->dateTime()
+          ->sortable()
+          ->toggleable(isToggledHiddenByDefault: true),
+        Tables\Columns\TextColumn::make('updated_at')
+          ->dateTime()
+          ->sortable()
+          ->toggleable(isToggledHiddenByDefault: true),
+        Tables\Columns\TextColumn::make('transactions_id')
+          ->numeric()
+          ->sortable()
+          ->toggleable(isToggledHiddenByDefault: true),
+        Tables\Columns\TextColumn::make('food.name')
+          ->label('Food Name')
+          ->sortable()
+          ->searchable(),
+        Tables\Columns\TextColumn::make('quantity')
+          ->numeric()
+          ->sortable(),
+        Tables\Columns\TextColumn::make('price')
+          ->money('IDR')
+          ->sortable(),
+        Tables\Columns\TextColumn::make('subtotal')
+          ->numeric()
+          ->money('IDR')
+          ->sortable(),
+      ])
+      ->filters([
+        //
+      ])
+      ->actions([])
+      ->bulkActions([]);
+  }
+
+  public static function getRelations(): array
+  {
+    return [
+      //
+    ];
+  }
+
+  public static function getPages(): array
+  {
+    return [
+      'index' => Pages\ListTransactionItems::route('/'),
+    ];
+  }
 }
